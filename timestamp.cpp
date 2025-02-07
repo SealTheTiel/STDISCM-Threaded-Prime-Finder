1f440b21cc9a1cc08d8dee09342103465de224c5
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2025-02-07 15:04:19 +0800

    refactor to use proper class declarations

--- timestamp.cpp
+++ timestamp.cpp
@@ -1,1 +1,36 @@
 #include "timestamp.h"
+
+#include <string>
+#include <time.h>
+#include <chrono>
+using namespace std;
+using namespace chrono;
+
+timestamp::timestamp(int day, int month, int year, int hour, int minute, int second, int millisecond) {
+	this->day = day;
+	this->month = month;
+	this->year = year;
+	this->hour = hour;
+	this->minute = minute;
+	this->second = second;
+	this->nanosecond = millisecond;
+};
+
+timestamp::timestamp() {
+	time_t time_tNow = time(0);
+	struct tm tmNow;
+	localtime_s(&tmNow, &time_tNow);
+	this->day = tmNow.tm_mday;
+	this->month = tmNow.tm_mon + 1;
+	this->year = tmNow.tm_year + 1900;
+	this->hour = tmNow.tm_hour;
+	this->minute = tmNow.tm_min;
+	this->second = tmNow.tm_sec;
+	this->nanosecond = duration_cast<nanoseconds>(system_clock::now().time_since_epoch()).count() % 1000;
+};
+
+string timestamp::getString() {
+	char temp[32];
+	sprintf_s(temp, sizeof(temp), "[%02d/%02d/%04d %02d:%02d:%02d.%09d]", this->day, this->month, this->year, this->hour, this->minute, this->second, this->nanosecond);
+	return string(temp);
+}
--- timestamp.h
+++ timestamp.h
@@ -1,9 +1,7 @@
 #pragma once
 #include <string>
-#include <time.h>
-#include <chrono>
+
 using namespace std;
-using namespace chrono;
 class timestamp
 {
 public:
@@ -13,35 +11,12 @@ public:
 	int hour;
 	int minute;
 	int second;
-	int millisecond;
+	int nanosecond;
 	
-	timestamp(int day, int month, int year, int hour, int minute, int second, int millisecond) {
-		this->day = day;
-		this->month = month;
-		this->year = year;
-		this->hour = hour;
-		this->minute = minute;
-		this->second = second;
-		this->millisecond = millisecond;
-	}
+	timestamp(int day, int month, int year, int hour, int minute, int second, int nanosecond);
 
-	timestamp() {
-		time_t time_tNow = time(0);
-		struct tm tmNow;
-		localtime_s(&tmNow, &time_tNow);
-		this->day = tmNow.tm_mday;
-		this->month = tmNow.tm_mon + 1;
-		this->year = tmNow.tm_year + 1900;
-		this->hour = tmNow.tm_hour;
-		this->minute = tmNow.tm_min;
-		this->second = tmNow.tm_sec;
-		this->millisecond = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() % 1000;
-	}
+	timestamp();
 
-	string getString() {
-		char temp[26];
-		sprintf_s(temp, sizeof(temp), "[%02d/%02d/%04d %02d:%02d:%02d.%03d]", this->day, this->month, this->year, this->hour, this->minute, this->second, this->millisecond);
-		return string(temp);
-	}
+	string getString();
 };
 

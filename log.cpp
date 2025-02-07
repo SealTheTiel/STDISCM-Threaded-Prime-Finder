6d39d5025e48529c74b4714da4f878b403c53a60
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2025-02-07 15:03:56 +0800

    refactor to use proper class declarations

--- log.cpp
+++ log.cpp
@@ -1,1 +1,20 @@
 #include "log.h"
+#include "timestamp.h"
+
+#include <string>
+
+logEntry::logEntry() {
+	this->threadId = 0;
+	this->message = "";
+	this->time = timestamp();
+};
+
+logEntry::logEntry(uint64_t threadId, string message) {
+	this->threadId = threadId;
+	this->message = message;
+	this->time = timestamp();
+};
+
+string logEntry::getString() {
+	return this->time.getString() + "  [Thread: " + to_string(this->threadId) + "]: \t" + this->message;
+}
--- log.h
+++ log.h
@@ -2,25 +2,16 @@
 #include <string>
 #include "timestamp.h"
 
+using namespace std;
 class logEntry {
 public:
 	string message;
 	uint64_t threadId;
 	timestamp time;
 
-	logEntry() {
-		this->threadId = 0;
-		this->message = "";
-		this->time = timestamp();
-	}
+	logEntry();
 
-	logEntry(uint64_t threadId, string message) {
-		this->threadId = threadId;
-		this->message = message;
-		this->time = timestamp();
-	}
+	logEntry(uint64_t threadId, string message);
 	
-	string getString() {
-		return this->time.getString() + "  [Thread: " + to_string(this->threadId) + "]: \t" + this->message;
-	}
+	string getString();
 };
